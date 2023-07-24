f5e364d3ca2aa383a78757f9d610a5eee561c499
Harika_Jeedigunta <84392557+Harikajeedigunta@users.noreply.github.com>
2023-07-24 21:09:42 +0530

    Create Print Linked List elements.cpp

---
+++ Print Linked List elements.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+    public:
+    void display(Node *head)
+    {
+      //your code goes here
+      if(head==NULL)
+      {
+          return;
+      }
+      cout<<head->data<<" ";
+      display(head->next);
+      
+    }
+};
